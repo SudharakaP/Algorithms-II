0454238c1d9931687339d116d526385efac2b2b5
SudharakaP <sudharakap@mun.ca>
2019-05-09 14:04:19 -0700

    Basic structure for completion of all the methods (needs verification and performance testing)

--- wordnet/SAP.java
+++ wordnet/SAP.java
@@ -4,34 +4,81 @@
  *  Description: Structure to hold the Shortest Ancestral Path
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.BreadthFirstDirectedPaths;
 import edu.princeton.cs.algs4.Digraph;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class SAP {
+    private Digraph digraph;
 
     // constructor takes a digraph (not necessarily a DAG)
-    public SAP(Digraph G) {}
+    public SAP(Digraph G) {
+        this.digraph = G;
+    }
 
     // length of shortest ancestral path between v and w; -1 if no such path
     public int length(int v, int w) {
-        return 0;
+        BreadthFirstDirectedPaths breadthFirstDirectedPaths = new BreadthFirstDirectedPaths(digraph, v);
+        if (breadthFirstDirectedPaths.hasPathTo(w))
+            return breadthFirstDirectedPaths.distTo(w);
+        else
+            return -1;
     }
 
     // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
     public int ancestor(int v, int w) {
-        return 0;
+        BreadthFirstDirectedPaths g1 = new BreadthFirstDirectedPaths(digraph, v);
+        BreadthFirstDirectedPaths g2 = new BreadthFirstDirectedPaths(digraph, w);
+        int vertex = digraph.V();
+        int minLength = Integer.MAX_VALUE;
+        int result = -1;
+        for (int i = 0; i < vertex; i++) {
+            if (g1.hasPathTo(i) && g2.hasPathTo(i)) {
+                int cur = g1.distTo(i) + g2.distTo(i);
+                if (cur < minLength) {
+                    minLength = cur;
+                    result = i;
+                }
+            }
+        }
+        if (minLength == Integer.MAX_VALUE) return -1;
+        return result;
     }
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        BreadthFirstDirectedPaths breadthFirstDirectedPaths = new BreadthFirstDirectedPaths(digraph, v);
+        int lengthShortest = Integer.MAX_VALUE;
+        for (Integer vertex: w){
+            if (breadthFirstDirectedPaths.hasPathTo(vertex) && breadthFirstDirectedPaths.distTo(vertex) < lengthShortest)
+                    lengthShortest = breadthFirstDirectedPaths.distTo(vertex);
+        }
+        if (lengthShortest == Integer.MAX_VALUE)
+            return -1;
+        else
+            return lengthShortest;
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        return 0;
+        BreadthFirstDirectedPaths BFDPathsFromV = new BreadthFirstDirectedPaths(digraph, v);
+        BreadthFirstDirectedPaths BFDPathsFromW = new BreadthFirstDirectedPaths(digraph, w);
+        int numberOfVertices = digraph.V();
+        int minLength = Integer.MAX_VALUE;
+        int result = -1;
+        for (int i = 0; i < numberOfVertices; i++) {
+            if (BFDPathsFromV.hasPathTo(i) && BFDPathsFromW.hasPathTo(i)) {
+                int cur = BFDPathsFromV.distTo(i) + BFDPathsFromW.distTo(i);
+                if (cur < minLength) {
+                    minLength = cur;
+                    result = i;
+                }
+            }
+        }
+        if (minLength == Integer.MAX_VALUE) return -1;
+        return result;
     }
 
     // do unit testing of this class
--- wordnet/WordNet.java
+++ wordnet/WordNet.java
@@ -5,43 +5,83 @@
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.Digraph;
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class WordNet {
         private Digraph digraph;
+        private List<String> nounList;
+        private List<String> hypernymList;
+        private Set<String> wordList;
+        private Map<Integer, String> nounMap;
+        SAP sap;
 
         // constructor takes the name of the two input files
         public WordNet(String synsets, String hypernyms) {
             if (synsets == null || hypernyms == null)
                 throw new IllegalArgumentException();
+            wordList = new HashSet<>();
+            nounMap = new HashMap<>();
+            nounList = new ArrayList<>(1);
+            hypernymList = new ArrayList<>(1);
+            In inputNouns = new In(synsets);
+            In inputHypernyms = new In(hypernyms);
+            while (inputNouns.hasNextLine()) {
+                String line = inputNouns.readLine();
+                String[] lineArray = line.split(",");
+                int index = Integer.parseInt(lineArray[0]);
+                String noun = lineArray[1];
+                nounList.add(line);
+                for (String word: noun.split(" "))
+                    wordList.add(word);
+                nounMap.put(index, noun);
+            }
+            while (inputHypernyms.hasNextLine())
+                hypernymList.add(inputHypernyms.readLine());
+            digraph = new Digraph(nounList.size());
+            for (String hypernym: hypernymList){
+                String[] vertices = hypernym.split(",");
+                for (int i = 1; i < vertices.length; i++)
+                    digraph.addEdge(Integer.parseInt(vertices[i]), Integer.parseInt(vertices[0]));
+            }
+            sap = new SAP(digraph);
         }
 
         // returns all WordNet nouns
         public Iterable<String> nouns() {
-            return null;
+            return nounList;
         }
 
         // is the word a WordNet noun?
         public boolean isNoun(String word) {
             if (word == null)
                 throw new IllegalArgumentException();
-            return false;
+            return wordList.contains(word);
         }
 
-        // distance between nounA and nounB (defined below)
+        // distance between nounA and nounB
         public int distance(String nounA, String nounB) {
-            if (nounA == null || nounB == null)
+            if (nounA == null || nounB == null || !isNoun(nounA) || isNoun(nounB))
                 throw new IllegalArgumentException();
-
-            return 0;
+            int nounVertexIndexA = Integer.parseInt(nounA.split(",")[0]);
+            int nounVertexIndexB = Integer.parseInt(nounB.split(",")[0]);
+            return sap.length(nounVertexIndexA, nounVertexIndexB);
         }
 
         // a synset (second field of synsets.txt) that is the common ancestor of nounA and nounB
-        // in a shortest ancestral path (defined below)
+        // in a shortest ancestral path
         public String sap(String nounA, String nounB) {
-            if (nounA == null || nounB == null)
+            if (nounA == null || nounB == null || !isNoun(nounA) || isNoun(nounB))
                 throw new IllegalArgumentException();
-
-            return null;
+            int nounVertexIndexA = Integer.parseInt(nounA.split(",")[0]);
+            int nounVertexIndexB = Integer.parseInt(nounB.split(",")[0]);
+            return nounMap.get(sap.ancestor(nounVertexIndexA, nounVertexIndexB));
         }
 
         // do unit testing of this class
